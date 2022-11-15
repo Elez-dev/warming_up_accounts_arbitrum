96d7b1d50c436037528b0831a42283e85f7a3a46
Maikl iz University <107795811+MaxTatar228@users.noreply.github.com>
2022-11-15 21:51:03 +0300

    version 3

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import threading
 
 factor = 1
 number_of_threads = 1
+number_of_repetitions = 2
+flag = 0
 
 RPC = {
     # '1': '',
@@ -19,6 +21,9 @@ RPC = {
     '42161': 'https://arb1.arbitrum.io/rpc',
     # '43114': '',  # https://support.avax.network/en/articles/4626956-how-do-i-set-up-metamask-on-avalanche
 }
+
+flag1 = False
+flag1_usdc = False
 
 swaps_1inch = [
     {'address': '0xd3f1Da62CAFB7E7BC6531FF1ceF6F414291F03D3',
@@ -39,7 +44,15 @@ swaps_1inch = [
 
     {'address': '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1',
      'symbol': 'DAI',
-     'amount': (round(random.uniform(0.0006, 0.00091), 8))*factor}
+     'amount': (round(random.uniform(0.0006, 0.00091), 8))*factor},
+
+    {'address': '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f',
+     'symbol': 'WBTC',
+     'amount': (round(random.uniform(0.0006, 0.00091), 8)) * factor},
+
+    {'address': '0xd4d42F0b6DEF4CE0383636770eF773390d85c61A',
+     'symbol': 'SUSHI',
+     'amount': (round(random.uniform(0.0006, 0.00091), 8)) * factor}
 ]
 
 
@@ -84,9 +97,10 @@ def inch_swap(_privatekey, _amount_to_swap, fromTokenAddress, _to_symbol):
         cprint(f'\n>>> Ошибка | {address_wallet} | {_to_symbol} | {error}', 'red')
 
 
-def inch_swap_approve(_privatekey, amount, _fromTokenAddress, _to_symbol):
+def inch_swap_approve(_privatekey, _fromTokenAddress, _to_symbol):
     account = web3.eth.account.privateKeyToAccount(_privatekey)
     address_wallet = account.address
+    amount = 10*(10**50)
     try:
         url = f'https://api.1inch.io/v4.0/42161/approve/transaction?tokenAddress={_fromTokenAddress}&amount={amount}'
         json_data = get_api_call_data(url)
@@ -109,27 +123,43 @@ def inch_swap_approve(_privatekey, amount, _fromTokenAddress, _to_symbol):
 
 
 def inch_swap_sell(_privatekey, fromTokenAddress, to_symbol_):
-    global to_token_address
+    global to_token_address, flag, flag1, flag1_usdc
     account = web3.eth.account.privateKeyToAccount(_privatekey)
     address_wallet = account.address
     try:
-        if to_symbol_ == 'DBL':
-            abi = ABI.ABI_DBL
-        elif to_symbol_ == 'LPT':
-            abi = ABI.ABI_LPT
-        elif to_symbol_ == 'USDT':
+        if to_symbol_ == 'USDT':
             abi = ABI.ABI_USDT
         elif to_symbol_ == 'USDC':
             abi = ABI.ABI_USDT
         elif to_symbol_ == 'DAI':
             abi = ABI.ABI_DAI
+        elif to_symbol_ == 'WBTC':
+            abi = ABI.ABI_WBTC
+        elif to_symbol_ == 'SUSHI':
+            abi = ABI.ABI_SUSHI
         else:
             return
         token = web3.eth.contract(address=fromTokenAddress, abi=abi)  # declaring the token contract
         token_balance = token.functions.balanceOf(address_wallet).call()
         amount = token_balance
-        inch_swap_approve(_privatekey, amount, fromTokenAddress, to_symbol_)
-        time.sleep(random.randint(10, 15))
+        if to_symbol_ == 'USDC' and flag1_usdc is True:
+            _1inchurl = f'https://api.1inch.io/v4.0/42161/swap?fromTokenAddress={fromTokenAddress}&toTokenAddress=\
+{to_token_address}&amount={amount}&fromAddress={address_wallet}&slippage=3'
+            json_data = get_api_call_data(_1inchurl)
+            nonce = web3.eth.getTransactionCount(address_wallet)
+            tx = json_data['tx']
+            tx['nonce'] = nonce
+            tx['to'] = Web3.toChecksumAddress(tx['to'])
+            tx['gasPrice'] = int(tx['gasPrice'])
+            tx['value'] = int(tx['value'])
+            signed_tx = web3.eth.account.signTransaction(tx, _privatekey)
+            tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+            cprint(f'\n>>> Sell {to_symbol_} : https://arbiscan.io/tx/{web3.toHex(tx_hash)}', 'green')
+            print(f'    {address_wallet}')
+            return
+        if flag == 0 and flag1 is False:
+            inch_swap_approve(_privatekey, fromTokenAddress, to_symbol_)
+            time.sleep(random.randint(10, 15))
         _1inchurl = f'https://api.1inch.io/v4.0/42161/swap?fromTokenAddress={fromTokenAddress}&toTokenAddress=\
 {to_token_address}&amount={amount}&fromAddress={address_wallet}&slippage=3'
         json_data = get_api_call_data(_1inchurl)
@@ -160,18 +190,26 @@ if __name__ == "__main__":
     to_token_address = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"  # ETH
 
     def main():
+        global flag1, flag1_usdc
         while keys_list:
             privatekey = keys_list.pop(0)
             random.shuffle(swaps_1inch)
             cprint(f'\n=============== start : {privatekey} ===============', 'white')
-
-            for swap in swaps_1inch:
-                amount_to_swap = swap['amount']
-                from_token_address = swap['address']
-                to_symbol = swap['symbol']
-                inch_swap(privatekey, amount_to_swap, from_token_address, to_symbol)
-                time.sleep(random.randint(20, 30))
-                inch_swap_sell(privatekey, from_token_address, to_symbol)
+            inch_swap_sell(privatekey, '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8', 'USDC')
+            time.sleep(random.randint(20, 30))
+            flag1_usdc = True
+            for _ in range(number_of_repetitions):
+                for swap in swaps_1inch:
+                    amount_to_swap = swap['amount']
+                    from_token_address = swap['address']
+                    to_symbol = swap['symbol']
+                    inch_swap(privatekey, amount_to_swap, from_token_address, to_symbol)
+                    time.sleep(random.randint(20, 30))
+                    inch_swap_sell(privatekey, from_token_address, to_symbol)
+                    time.sleep(random.randint(20, 30))
+                flag1 = 1
+            amount_usdc = ((round(random.uniform(0.0006, 0.00091), 8))*factor)
+            inch_swap(privatekey, amount_usdc, '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8', 'USDC')
 
     for i in range(number_of_threads):
         thred = threading.Thread(target=main).start()
